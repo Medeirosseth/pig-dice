b359a9658df921b0bad7c0384b5f88fe48563208
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-07 10:42:20 -0700

    add all necessary folders/files and project outline
    Co-authored-by: Tori West <victoria_west@ymail.com>
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+
+
+
+
+// Business logic
+// one constructor for each play (2 players)
+// create a prototype for rolling dice 
+// create a prototype to keep score during a turn - calls rolling dice until hold or 1 is reached, then calls add score if score is positive
+// create a add score protoype (UI logic hold button)
+// create prototype method that randomly generates what player goes first 
+// 
+// 
+// 
+// player1
+// Score: score 
+// total: total 
+// conditional when total score of 100 is met, player is declared as winner 
+
+
+// UI logic
+// section for each player
+// roll and hold button  
+// append total score beneath player

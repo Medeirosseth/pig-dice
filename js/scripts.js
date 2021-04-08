7ed729131f1e9c0fb4ddbe7177cd3568afe8584e
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-08 13:08:01 -0700

    add HTML layout
    Co-authored-by: Tori West <victoria_west@ymail.com>
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Business logic
 // one constructor for each play (2 players)
-function Players(name, score, total){
+function Player(name, score, total){
   this.name = name;
   this.scoreThisRound = score;
   this.totalScore = total;
@@ -10,10 +10,12 @@ function Players(name, score, total){
 
 }
 
+
 function allPlayers() {
-  this.players = {}
+  this.players = []
   this.currentId = 0;
 }
+
 
 
 allPlayers.prototype.assignId=function(){
@@ -23,10 +25,10 @@ allPlayers.prototype.assignId=function(){
 
 allPlayers.prototype.addPlayer=function(player){
  player.id = this.assignId();
- this.players[player.id] = player;
+ this.players.push(player);
 }
 
-Players.prototype.rollDice=function(){
+Player.prototype.rollDice=function(){
  let number = Math.floor(Math.random() * 6) + 1; {
   if( number ===1){
     this.myTurn=false;
@@ -38,7 +40,7 @@ Players.prototype.rollDice=function(){
 }
 };
 
-Players.prototype.endTurn=function(number){
+Player.prototype.endTurn=function(number){
   if(number===1){
     this.scoreThisRound = 0;
   } else {
@@ -47,30 +49,35 @@ Players.prototype.endTurn=function(number){
   } 
 }
 
-Players.prototype.storeScore=function(number){
+Player.prototype.storeScore=function(number){
   this.scoreArray.push(number);
   this.scoreThisRound += number;
 }
 
-let pvp = new allPlayers();
-let player1 = new Players("John", 0, 0);
-let player2 = new Players("Jane", 0, 0);
-pvp.addPlayer(player1);
-pvp.addPlayer(player2);
-console.log(player1.id);
-console.log(player2.id);
-// //John's turn starts
-// let number = player.rollDice(); //get a single dice roll result
-// player.storeScore(number); //store that result in ScoreArray and scoreThisRound
-// while(player.myTurn && player.hold===false) { //as long as they don't get a 1 and don't decide to hold, keep rolling
-//   number = player.rollDice();
-//   player.storeScore(number);
-//   player.hold=true;  //player choses to hold after second roll
-// }
-// player.hold=false; //reset hold
-// player.endTurn(number) //check if the last number they rolled was a 1. If not, that means you chose 'hold' and the program should add scoreThisRound to your totalScore
-// //set other player's turn to true
+function gameTurn(currentPlayer){ //John's turn starts
+  let number = currentPlayer.rollDice(); //get a single dice roll result
+  currentPlayer.storeScore(number); //store that result in ScoreArray and scoreThisRound
+  while(currentPlayer.myTurn && currentPlayer.hold===false) { //as long as they don't get a 1 and don't decide to hold, keep rolling
+    number = currentPlayer.rollDice(); //roll again
+    currentPlayer.storeScore(number); //store that result in ScoreArray and scoreThisRound
+    currentPlayer.hold=true;  //player choses to hold after second roll
+  }
+  player.myTurn=false;
+  player.hold=false; //reset hold
+  player.endTurn(number) //check if the last number they rolled was a 1. If not, that means you chose 'hold' and the program should add scoreThisRound to your totalScore
+}
 
+function playGame(allPlayers) {
+  let player1=allPlayers.players[0];
+  let player2=allPlayers.players[1];
+  if (player1.myTurn){
+    gameTurn(player1);
+  }
+  //let player1 = Object.keys(player1.id)
+}
+//set other player's turn to true
+//if the current player's id=1, set new current player to player with id 2
+//if the current player's id=2, set new current player to player with id 1
 // create a prototype to update scoreThisRound during a turn, adding to score or changing it to zero
 // create a prototype to end turn
 // create a add score protoype (UI logic hold button)
@@ -83,6 +90,13 @@ console.log(player2.id);
 // total: total 
 // conditional when total score of 100 is met, player is declared as winner 
 
+//TEST LOGIC
+let pvp = new allPlayers();
+let playerJohn = new Player("John", 0, 0);
+let playerJane = new Player("Jane", 0, 0);
+pvp.addPlayer(playerJohn);
+pvp.addPlayer(playerJane);
+playGame(pvp);
 
 // UI logic
 // section for each player

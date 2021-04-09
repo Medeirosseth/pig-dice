017d1f7990a2adb5e8fff33615ba62e6c83b377a
Medeirosseth <79333650+Medeirosseth@users.noreply.github.com>
2021-04-08 20:42:35 -0700

    add functionality to hold button
    Co-authored-by: Tori West <victoria_west@ymail.com>
    Co-authored-by: Cristina Plesa <cristinaplesa8@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,17 +6,12 @@ function Player(name, score, total){
   this.totalScore = total;
   this.scoreArray = [];
   this.myTurn = true;
-
-
 }
-
 
 function allPlayers() {
   this.players = []
   this.currentId = 0;
 }
-
-
 
 allPlayers.prototype.assignId=function(){
   this.currentId += 1;
@@ -28,9 +23,22 @@ allPlayers.prototype.addPlayer=function(player){
  this.players.push(player);
 }
 
+allPlayers.prototype.getCurrentPlayerIndex=function() {
+  let player1=this.players[0];
+  let player2=this.players[1];
+  if(player1.myTurn){
+    return 0;
+  }
+  else {
+    return 1;
+  }
+}
+
 Player.prototype.rollDice=function(){
  let number = Math.floor(Math.random() * 6) + 1; {
   if( number ===1){
+    this.scoreThisRound = 0;
+    this.scoreArray = [];
     this.myTurn=false;
     return number;
   } else {
@@ -40,14 +48,11 @@ Player.prototype.rollDice=function(){
 }
 };
 
-Player.prototype.endTurn=function(number){
-  if(number===1){
-    this.scoreThisRound = 0;
-  } else {
+Player.prototype.endTurn=function(){
     this.totalScore += this.scoreThisRound;
-    this.scoreThisRound = 0
-  } 
-}
+    this.scoreThisRound = 0;
+    this.scoreArray = [];
+} 
 
 Player.prototype.storeScore=function(number){
   this.scoreArray.push(number);
@@ -56,25 +61,70 @@ Player.prototype.storeScore=function(number){
 
 function gameTurn(currentPlayer){ //John's turn starts
   let number = currentPlayer.rollDice(); //get a single dice roll result
-  currentPlayer.storeScore(number); //store that result in ScoreArray and scoreThisRound
-  while(currentPlayer.myTurn && currentPlayer.hold===false) { //as long as they don't get a 1 and don't decide to hold, keep rolling
-    number = currentPlayer.rollDice(); //roll again
-    currentPlayer.storeScore(number); //store that result in ScoreArray and scoreThisRound
-    currentPlayer.hold=true;  //player choses to hold after second roll
-  }
-  player.myTurn=false;
-  player.hold=false; //reset hold
-  player.endTurn(number) //check if the last number they rolled was a 1. If not, that means you chose 'hold' and the program should add scoreThisRound to your totalScore
-}
+  if (currentPlayer.myTurn){
+    currentPlayer.storeScore(number);} //store that result in ScoreArray and scoreThisRound
+  // while(currentPlayer.myTurn && currentPlayer.hold===false) { //as long as they don't get a 1 and don't decide to hold, keep rolling
+  //   number = currentPlayer.rollDice(); //roll again
+  //   currentPlayer.storeScore(number); //store that result in ScoreArray and scoreThisRound
+  //   currentPlayer.hold=true;  //player choses to hold after second roll
+  // }
+  currentPlayer.myTurn=false;}
+  // currentPlayer.hold=false; //reset hold
+  // currentPlayer.endTurn(number) //check if the last number they rolled was a 1. If not, that means you chose 'hold' and the program should add scoreThisRound to your totalScore
+//}
+  
+
 
 function playGame(allPlayers) {
   let player1=allPlayers.players[0];
   let player2=allPlayers.players[1];
   if (player1.myTurn){
     gameTurn(player1);
+    return player1;
   }
-  //let player1 = Object.keys(player1.id)
 }
+
+let pvp = new allPlayers();
+  let player1Object = new Player("", 0, 0);
+  let player2Object = new Player("", 0, 0);
+  pvp.addPlayer(player1Object);
+  pvp.addPlayer(player2Object);
+
+// User interface logic
+$(document).ready(function() {
+  
+  $("#formName").submit(function(event) {
+    event.preventDefault();
+    const playerName1 = $("input#playerName1").val();
+    player1Object.name=playerName1;
+    const playerName2 = $("input#playerName2").val();
+    player2Object.name=playerName2;
+    $("#player1").text(playerName1);
+    $("#player2").text(playerName2);
+  });
+  $("#rollDice").click(function() {
+    let currentPlayer=playGame(pvp);
+    $("#scoreArray").text(currentPlayer.scoreArray);
+    $("#scoreThisRound").text(currentPlayer.scoreThisRound);
+  });
+  $("#hold").click(function() {
+    const index=pvp.getCurrentPlayerIndex();
+    const currentPlayer = pvp.players[index];
+    let holdDice = currentPlayer.endTurn();
+    $("#totalScore").text(holdDice.scoreThisRound);
+  })
+})
+
+
+// const rollDice = ("button#rollDice").valueOf()
+//   const 
+
+
+
+
+
+
+
 //set other player's turn to true
 //if the current player's id=1, set new current player to player with id 2
 //if the current player's id=2, set new current player to player with id 1
@@ -91,12 +141,7 @@ function playGame(allPlayers) {
 // conditional when total score of 100 is met, player is declared as winner 
 
 //TEST LOGIC
-let pvp = new allPlayers();
-let playerJohn = new Player("John", 0, 0);
-let playerJane = new Player("Jane", 0, 0);
-pvp.addPlayer(playerJohn);
-pvp.addPlayer(playerJane);
-playGame(pvp);
+
 
 // UI logic
 // section for each player
